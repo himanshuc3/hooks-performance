1f9e0ab90e88fab22c8b55cda666d90ba314bea6
Himanshu Chhabra <himichhabra14@gmail.com>
2025-04-04 02:00:08 +0530

    fix: indexing types

---
+++ src/utils/indexingTypes.ts
@@ -0,0 +1,27 @@
+interface Car {
+  make: string;
+  model: string;
+  year: number;
+  color: {
+    red: string;
+    green: string;
+    blue: string;
+  };
+}
+
+// NOTE:
+// 1. Indexing helps take a subtype out of an interface
+let carColor: Car["color"];
+let carColorRed: Car["color"]["red"];
+
+let carProperty: Car["color" | "year"];
+
+// NOTE:
+// 1. Mapped types in index signature
+type Fruit = {
+  name: string;
+  color: string;
+  quantity: Number;
+};
+
+type MyRecord = { [FruitKey in "apple" | "cherry"]: Fruit };
